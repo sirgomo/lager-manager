738359ff0576ae2f21274164fbc933a62d13d8fc
karo81 <wlodek_w@yahoo.de>
2022-10-16 13:26:57 +0200

    Dodano Artikel i generowanie ich

---
+++ src/pipes/artikelFlagspipes.ts
@@ -0,0 +1,19 @@
+import { ArgumentMetadata, BadRequestException, PipeTransform } from "@nestjs/common";
+import { artikelFlage } from "src/entity/artikelEntity";
+
+export class artikelflagsPipers implements PipeTransform{
+    readonly erlaubtesFlags : artikelFlage[] = [artikelFlage.ALK, artikelFlage.FASS, artikelFlage.SUSS];
+
+    transform(value : any, metadata : ArgumentMetadata) : any {
+        value = value.toUpperCase();
+        if(!this.isStatusValid(value)){
+            throw new BadRequestException (`$value ist falsch.`);
+        }
+        return value;
+    }
+    private isStatusValid(value: any) : boolean{
+       const index : number = this.erlaubtesFlags.indexOf(value);
+       return index === -1;
+    }
+
+}
